c537330b8c6f04033650ef005396889ad1e6491c
Marko Vujnovic <mv@becklyn.com>
2022-08-22 13:00:59 +0200

    Typed CommandBus::dispatch for Command interface

--- src/Commands/Application/CommandBus.php
+++ src/Commands/Application/CommandBus.php
@@ -2,11 +2,13 @@
 
 namespace Becklyn\Ddd\Commands\Application;
 
+use Becklyn\Ddd\Commands\Domain\Command;
+
 /**
  * @author Marko Vujnovic <mv@becklyn.com>
  * @since  2019-06-05
  */
 interface CommandBus
 {
-    public function dispatch($command): void;
+    public function dispatch(Command $command): void;
 }
